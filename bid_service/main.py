53d4a48404b99dddb6f5f187c8ac196d9907b3cf
Gabe Costanzo <gabriel.costanzo@ttu.edu>
2021-03-31 13:35:06 -0500

    update bid service

--- bid_service/main.py
+++ bid_service/main.py
@@ -2,7 +2,8 @@ import flask
 import pymongo
 from pymongo import MongoClient
 from flask import request
-import datetime
+from datetime import datetime
+import json
 
 
 cluster = MongoClient("mongodb+srv://nicolas:capstone1@cluster0.vfv8c.mongodb.net/capstone?retryWrites=true&w=majority")
@@ -20,39 +21,39 @@ class Bid():
         self.time = datetime.now()
 
     def convertToJSON(self):
-        return {"User": self.user, "Listing_ID": self.lid, "Bid_Amount": self.bid_amt, "Time": self.time}
+        return {"User": self.user, "Bid_Amount": self.bid_amt, "Time": self.time}
 
 # returns all bids associated with a listing
 @app.route('/bid_service/getBids', methods=['GET'])
 def getBids():
     if 'lid' in request.args:
-        lid = request.args.getlist('lid')
+        lid = int(request.args.getlist('lid')[0])
     else:
         return {"Success": False, "Error Message": "Invalid LID"}
 
     # returns list of bids for lid
     listing = collection.find_one({"_id": lid})
     bid_list = listing["Bids"]
-    return bid_list
+    return {"Success": True, "data": bid_list}
 
 # updates bid list of a listing if the new bid is higher than the previous
 @app.route('/bid_service/placeBid', methods=['GET'])
 def placeBid():
     # get user id
     if 'uid' in request.args:
-        uid = request.args.getlist('uid')
+        uid = request.args.getlist('uid')[0]
     else:
         return {"Success": False, "Error Message": "Invalid UID"}
 
     # get listing id
     if 'lid' in request.args:
-        lid = request.args.getlist('lid')
+        lid = int(request.args.getlist('lid')[0])
     else:
         return {"Success": False, "Error Message": "Invalid LID"}
 
     # get bid amount
     if 'bid_amt' in request.args:
-        bid_amt = request.args.getlist('bid_amt')
+        bid_amt = float(request.args.getlist('bid_amt')[0])
     else:
         return {"Success": False, "Error Message": "Invalid Bid Amount"}
 
@@ -64,17 +65,28 @@ def placeBid():
 
     # find highest bid that has been made on the product
     bid_list = listing["Bids"]
-    if bid_list is []:
-        highest_bid = -1
+    if bid_list == []:
+        bid_list.append(new_bid.convertToJSON())
+        print("=========================================================")
+        print(bid_list)
+        db.collection.update_one({'_id':lid},
+                                {'$set': {"Bids": bid_list}}, 
+                                upsert=True)
+
+        return {"Success": True}
     else:
         highest_bid = bid_list[-1]
 
     # update highest bid
     if bid_amt > highest_bid["Bid_Amount"]:
         bid_list.append(new_bid.convertToJSON())
-        collection.update_one({"_id": lid}, {"$set", {"Bids": bid_list}})
+        db.collection.update_one({'_id':lid},
+                                {'$set': {"Bids": bid_list}}, 
+                                upsert=True)
         # give Nick data of person who was previously out-bid
-        r = request.post("http://bugs.python.org/?uid=" + highest_bid.user + "&lid=" + lid)
+        #r = request.post("http://bugs.python.org/?uid=" + highest_bid.user + "&lid=" + lid)
         return {"Success": True}
     else:
         return {"Success": False, "Error Message": "Invalid Bid"}
+
+app.run()
