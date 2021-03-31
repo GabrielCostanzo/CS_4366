011135dc90212201ae4f84ae8e0c889bbfe022f0
Gabe Costanzo <gabriel.costanzo@ttu.edu>
2021-03-31 02:09:01 -0500

    add account management

--- webapp/drink_trader/auction/urls.py
+++ webapp/drink_trader/auction/urls.py
@@ -5,6 +5,8 @@ from . import views
 app_name = 'auction'
 urlpatterns = [
     path('create_account', views.create_account, name='create_account'),
+    path('create_account_submit', views.create_account_submit, name='create_account_submit'),
+    path('pending_verification', views.pending_verification, name='pending_verification'),
     path('login', views.login, name='login_detail'),
     path('login_submit', views.login_submit, name='login_submit'),
     path('listings', views.listings, name='listings'),
--- webapp/drink_trader/auction/views.py
+++ webapp/drink_trader/auction/views.py
@@ -2,10 +2,62 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.http import Http404
 from django.urls import reverse
 from django.shortcuts import render
+import requests
+import pprint
+
+class product():
+	def __init__(self, json_product):
+		self.alcohol_percentage = json_product["AlcoholPercentage"]
+		self.alcohol_type = json_product["AlcoholType"]
+		self.country_of_orgin = json_product["CountryOfOrigin"]
+		self.description = json_product["Description"]
+		self.drink_brand = json_product["DrinkBrand"]
+		self.drink_name = json_product["DrinkName"]
+		self.volume =  json_product["Volume"]
+		self.id = json_product["_id"]
+
+class listing():
+	def __init__(self, json_listing):
+		self.expiration = json_listing["Expiration"]
+		self.product = product(json_listing["Product"])
+		self.seller = json_listing["Seller"]
+		self.start_price = json_listing["StartPrice"]
+		self.status = json_listing["Status"]
+		self.terminating_price = json_listing["TerminatingPrice"]
+		#self.bids = json_listing[""] 
+		self.type = json_listing["Type"]
+		self.id = json_listing["_id"]
+
+def get_listings():
+	URL = "http://127.0.0.1:5000/getNumberOfListings?num=3"
+	r = requests.get(url = URL)
+	json_response = r.json()
+
+	listing_objects = []
+	for json_listing in json_response['data']:
+		pprint.pprint(json_listing)
+		listing_objects.append(listing(json_listing))
+
+	return listing_objects
+
 
 def create_account(request):
     context = {}
     return render(request, 'auction/create_account.html', context)
+
+def create_account_submit(request):
+
+	first_name = request.POST['first_name']
+	username = request.POST['username']
+	password = hash(request.POST['password'])
+
+	#TODO: call account service's create account function
+
+	return HttpResponseRedirect(reverse('auction:pending_verification'))
+
+def pending_verification(request):
+    context = {}
+    return render(request, 'auction/pending_verification.html', context)
 
 def login(request):
     context = {}
@@ -13,16 +65,20 @@ def login(request):
 
 def login_submit(request):
 	username = request.POST['username']
-	password = request.POST['password']
+	password = hash(request.POST['password'])
+
+	#TODO: call account service's login function
 
 	return HttpResponseRedirect(reverse('auction:listings'))
 
-
-
 def listings(request):
-    context = {}
-    return render(request, 'auction/listings.html', context)
+	listings = get_listings()
+	print("========================")
+	for listing in listings:
+		print(listing.start_price)
+	context = {"listings": listings}
+	return render(request, 'auction/listings.html', context)
 
 def listing_detail(request, listing_id):
-    context = {"listing_id": listing_id}
-    return render(request, 'auction/listing_detail.html', context)
+	context = {"listing_id": listing_id}
+	return render(request, 'auction/listing_detail.html', context)
